5da14825ec91e49cd28c3def022bb523035e1c5e
Warlock <warlockdn@gmail.com>
2018-04-26 07:41:55 +0530

    updated

--- src/app/pages/partners/listing/listing.component.ts
+++ src/app/pages/partners/listing/listing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
-
+import { ParamMap, Router, ActivatedRoute } from '@angular/router';
 import { PartnerService } from '../../../service/partner/partner.service';
 import { MenuManagerService } from '../../../service/menu-manager/menu-manager.service';
 
@@ -15,14 +14,28 @@ export class PartnerListingComponent implements OnInit {
   listingType: string = 'All';
 
   displayedColumns = ['partnerid', 'name', 'location', 'state', 'revenue', 'commissions', 'isActive', 'actions'];
-  dataSource: any;
+  dataSource: any;a
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private router: Router, private partner: PartnerService, private menuService: MenuManagerService) {  }
+  constructor(private router: Router, private partner: PartnerService, private menuService: MenuManagerService, private route: ActivatedRoute) {  }
 
   ngOnInit() {
     this.partner.getAllPartners().then((result) => {
-      this.dataSource = new MatTableDataSource<Element>(result.data);
+      const activePage = (this.route.snapshot.params.type).toString();
+      let partners = result.data;
+      let temp = [];
+      partners.map((partner) => {
+        if (activePage === 'all') {
+          temp.push(partner);
+        } else if (activePage === 'pending' && partner.isPending === true) {
+          temp.push(partner);
+        } else if (activePage === 'active' && partner.isActive === true) {
+          temp.push(partner);
+        } else {
+          throw new Error('Error traversing list');
+        }
+      })
+      this.dataSource = temp;
     }).catch((err) => {
       console.log(err);
     })    
