a8d939c6566227a489385bb9d9a0b5e23396f169
Warlock <warlockdn@gmail.com>
2018-04-27 08:37:29 +0530

    updated constants

--- src/app/service/constants/constants.service.ts
+++ src/app/service/constants/constants.service.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsService {
 
-  public static api = 'http://localhost:3000/';
-  public static auth = 'http://localhost:3000/auth/';
-  public static partner = 'http://localhost:3000/partner/'
+  // public static api = 'http://localhost:3000/';
+  // public static auth = 'http://localhost:3000/auth/';
+  // public static partner = 'http://localhost:3000/partner/'
 
-  // public static api = 'http://ec2-13-232-37-65.ap-south-1.compute.amazonaws.com/';
-  // public static auth = 'http://ec2-13-232-37-65.ap-south-1.compute.amazonaws.com/auth/';
-  // public static partner = 'http://ec2-13-232-37-65.ap-south-1.compute.amazonaws.com/partner/'
+  public static api = 'http://api.spazefood.xyz/';
+  public static auth = 'http://api.spazefood.xyz/auth/';
+  public static partner = 'http://api.spazefood.xyz/partner/'
 
   constructor() { }
 
