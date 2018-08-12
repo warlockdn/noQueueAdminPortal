706c6efbe3250c038f2d91395fecd2d1c073715b
Warlock <warlockdn@gmail.com>
2018-08-12 10:22:17 +0530

    update

--- src/app/service/constants/constants.service.ts
+++ src/app/service/constants/constants.service.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsService {
 
-  public static api = 'http://localhost:3000';
+  /* public static api = 'http://localhost:3000';
   public static auth = 'http://localhost:3000/auth';
-  public static partner = 'http://localhost:3000/partner';
+  public static partner = 'http://localhost:3000/partner'; */
 
-  /* public static api = 'https://api.spazefood.xyz'; 
+  public static api = 'https://api.spazefood.xyz'; 
   public static auth = 'https://api.spazefood.xyz/auth';
-  public static partner = 'https://api.spazefood.xyz/partner'; */
+  public static partner = 'https://api.spazefood.xyz/partner';
 
   public static imagecdn = 'https://res.cloudinary.com/ddiiq3bzl/image/upload/logo/'
   public static imageserve = `https://res.cloudinary.com/ddiiq3bzl/image/upload/fl_lossy,f_auto,w_400,h_400,f_auto,c_fill/`
